0c6a05c60d157b297c0beae572bf3418d482821a
Matt Gifford <me@matthewgifford.com>
2012-03-19 12:22:04 -0700

    Fixed an error in maps.js that caused a break in Firefox (and probably IE)

--- 00_start/extras/maps.js
+++ 00_start/extras/maps.js
@@ -1,9 +1,9 @@
-<script type="text/javascript">
 var breakpoint = 481,
   id = 'mapcontainer',
   viewportWidth = window.innerWidth;
 if (viewportWidth > breakpoint) {
-  var mapElement = document.createElement('iframe');
+  var mapElement = document.createElement('iframe'),
+      mapLink = document.getElementById('maplink');
   mapElement.id = 'map';
   mapElement.width = '300';
   mapElement.height = '300';
@@ -12,6 +12,5 @@ if (viewportWidth > breakpoint) {
   mapElement.marginheight = '0';
   mapElement.marginwidth = '0';
   mapElement.src = 'http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=334+NW+11th+Ave,+Portland,+OR+97209&aq=&sll=37.0625,-95.677068&sspn=58.164117,80.332031&vpsrc=0&ie=UTF8&hq=&hnear=334+NW+11th+Ave,+Portland,+Oregon+97209&t=m&ll=45.525472,-122.68218&spn=0.01804,0.025749&z=14&output=embed';
-  document.getElementById(id).insertBefore(mapElement,maplink);
+  document.getElementById(id).insertBefore(mapElement,mapLink);
 }
-</script>
